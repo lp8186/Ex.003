82d91e4935782e75d7d4024496b232902d6b1052
liadp <lp8186@bs.amalnet.k12.il>
2020-02-10 19:17:07 +0200

    Working!

---
+++ app/src/main/java/com/example/ex003/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ex003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn= (Button) findViewById(R.id.btn);
+        count=0;
+    }
+
+    public void push(View view) {
+        count= count+1;
+        if (count!=6) {
+            btn.setText("This is a click number" + count);
+        }
+        else {
+            btn.setText("Enough to click. Go to new start!");
+            count=0;
+        }
+        }
+    }
+
